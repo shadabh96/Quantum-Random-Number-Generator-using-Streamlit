f70038ed5bc91ef47f237382fd4f4d5f9df93c48
Shadab Hussain <shadab.entrepreneur@outlook.com>
2021-02-01 04:59:54 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
-sudo add-apt-repository universe
-sudo apt-get install apt-transport-https
-sudo apt-get update
+wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-5.0
+  
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-5.0
+  
+sudo apt-get install -y dotnet-runtime-5.0
 
 export PATH="$PATH:$HOME/.dotnet"
 
